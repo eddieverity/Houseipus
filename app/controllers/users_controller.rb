9aabb59003743c6ef0d643e046e341a810ce29f6
Peter Herman <peter.b.herman@gmail.com>
2017-05-28 11:25:05 -0500

    added user login and reg

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+
+    def signin
+    end
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to '/'
+        else
+            flash[:register] = @user.errors.full_messages
+            redirect_to 'users/signin'
+        end
+    end
+    
+    def login
+        @user = User.find_by('email = ?', params[:user][:email])
+
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to '/'
+        else
+            flash[:login] = ["Invalid Email or Password"]
+            redirect_to 'users/signin'
+        end
+    end
+
+    def logout
+        reset_session
+        redirect_to '/'
+    end
+
+private
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'houses#index'
 
+  get 'users/signin' => 'users#signin'
+
+  post 'users/create'
+
+  post 'users/login'
+
+  get 'users/logout'
+
 end
