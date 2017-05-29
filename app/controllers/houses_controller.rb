0ea66f4ad72a63eff6ae9d27cbd8ed8c50c43bdd
Peter Herman <peter.b.herman@gmail.com>
2017-05-29 10:56:54 -0500

    got woking demo of search by zip

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,2 +1,18 @@
 class HousesController < ApplicationController
+
+    require 'net/http'
+    require 'json'
+
+    def maptest
+        @url = 'https://maps.googleapis.com/maps/api/geocode/json?address=60640&sensor=false'
+        @uri = URI(@url)
+        @response = Net::HTTP.get(@uri)
+        @data = JSON.parse(@response)
+
+        @lat = @data['results'][0]['geometry']['location']['lat']
+        @lng = @data['results'][0]['geometry']['location']['lng']
+
+        puts @latlong
+    end
+    
 end
