69e716ec00d9a543c0a00a3ff0b06eea4cce0602
Peter Herman <peter.b.herman@gmail.com>
2017-05-31 16:25:28 -0500

    Rental listings can be added and show up in search

--- app/models/rental_listing.rb
+++ app/models/rental_listing.rb
@@ -4,12 +4,12 @@ class RentalListing < ApplicationRecord
     [address, street, unit, city, state, zip].compact.join(',')
   end
   
-  geocoded_by :address
-  after_validation :geocode
+  geocoded_by :full_address
+
+  after_validation :geocode, if: ->(obj){ obj.full_address.present? and obj.address_changed? } 
 
   has_one :rental_image, dependent: :destroy
   has_many :rental_favorites, dependent: :destroy
-
 
   acts_as_mappable :lat_column_name => :latitude, :lng_column_name => :longitude
 
