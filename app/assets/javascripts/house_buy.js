61202595b25da7ab251937c1065b0165c894c57e
Peter Herman <peter.b.herman@gmail.com>
2017-06-01 15:22:27 -0500

    Fixed but where map didn't show up if there were no results

--- app/assets/javascripts/house_buy.js
+++ app/assets/javascripts/house_buy.js
@@ -9,14 +9,41 @@ var query = split[split.length-1]
 var listings;
 
 var fullquery = function(){
+    
     $.get(query +'.json', function(data){
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 13,
-            center: {lat: data[0].latitude, lng: data[0].longitude}
-        });
+
+        if (data.length > 0){ //checks if there are results on the map
+        //make the map
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 13,
+                center: {lat: data[0].latitude, lng: data[0].longitude}
+            });
+        }
+        else{
+            var googleurl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + query + "&key=AIzaSyDEIuPwq4UmLFZ-zqDXmqP1NI54lJhXllY"
+
+            $.get(googleurl, function(gdata){
+
+                var maplatlong = gdata.results[0].geometry.location;
+
+                console.log(maplatlong)
+
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 12,
+                    center: maplatlong
+                });
+
+            })
+        }
+
+        //variables to be used in future for loop
         var markers = [];
         var infowindows = [];
+
+        //this is the aforementioned for loop that will generate things on map
         for (var i = 0; i < data.length; i++){
+
+            //this part makes markers for every listing in query results
             markers[i] = new google.maps.Marker({
                 animation: google.maps.Animation.DROP,
                 position: {lat: data[i].latitude, lng: data[i].longitude },
@@ -25,7 +52,10 @@ var fullquery = function(){
                 id: i
             }) // close of marker creator
 
+            //2 types of markers for listings that have or do not have images
             if (data[i].image){
+
+                //generates infowindow for listings that have images
                 infowindows[i] = new google.maps.InfoWindow({
                     content: "<div class='infobox'>"
                     + "<a href='/listings/sale/" + data[i].id + "'><img src='" + data[i].image.gallery[0].url + "' /></a><br>" 
@@ -37,8 +67,10 @@ var fullquery = function(){
                     + "<input type='hidden' name='authenticity_token' value='<%= form_authenticity_token %>'>"
                     +"<input type='submit' value='Add to Favorites'></form>"
                     + "</div>"
-                });
+                    });
                 }
+
+                //generates infowindow for listings that DO NOT have images
                 else{
                     infowindows[i] = new google.maps.InfoWindow({
                     content: "<div class='infobox'>"
@@ -52,14 +84,17 @@ var fullquery = function(){
                     + "</div>"
                     });
                 }
+
+                //allows infowindow to open on click
                google.maps.event.addListener(markers[i], 'click', function () {
                     infowindows[this.id].open(map, markers[this.id]);
                 });
 
         } //close of for loop
         
-    })
-}
+    }) // end of AJAX get request
+
+} // end of fullquery function
 
 fullquery();
 
