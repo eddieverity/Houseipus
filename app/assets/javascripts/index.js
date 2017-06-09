1a731b2a9f867a78374b6774d8eb8ab319869cb7
Edward Verity <eddieverity@gmail.com>
2017-06-09 11:23:08 -0500

    testing fade for imgs

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,28 +1,39 @@
 $(document).ready(function(){
           if ($("#buy").click(function() {
+            //test fade
+            $(".hero").fadeOut(200, function(){
               $(".hero").css(
                 {'background': 'url("/assets/hero.jpg")',
                 'background-repeat': 'no-repeat',
                 'background-position': 'center',
                 'background-size': 'cover' 
-                });
+                })
+                $(".hero").fadeIn(200)
+            })
+            //testfade
           }));
           
           if ($("#rent").click(function() {
+            $(".hero").fadeOut(200, function(){
               $(".hero").css(
                   {'background': 'linear-gradient(rgba(0, 0, 0, 0.45),rgba(0, 0, 0, 0.45)), url("/assets/beachhome.jpg")',
                   'background-repeat': 'no-repeat',
                   'background-position': 'center',
                   'background-size': 'cover',
                 });
+              $(".hero").fadeIn(200)
+            })
           }));
 
           if ($("#sell").click(function() {
+            $(".hero").fadeOut(200, function(){
               $(".hero").css(
                   {'background': 'linear-gradient(rgba(0, 0, 0, 0.45),rgba(0, 0, 0, 0.45)), url("/assets/pymhouse.png")',
                   'background-repeat': 'no-repeat',
                   'background-position': 'center',
                   'background-size': 'cover' 
                 });
+              $(".hero").fadeIn(200)
+            }) 
           }));
     });
